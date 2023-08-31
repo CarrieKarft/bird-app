72bf025d905dc1f113c8610e1756e3a9ecadd8cb
Carrie Kraft <carriekarft@gmail.com>
2023-08-31 16:10:37 -0700

    Add show action

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,11 @@ class BirdsController < ApplicationController
     def index
         render json: Bird.all
     end
+
+    def show
+        bird = Bird.find(params[:id])
+        render json: bird
+    rescue ActiveRecord::RecordNotFound
+        render json: "Bird not found", status: :not_found
+    end
 end
